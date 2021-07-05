68ce35f36111556076586cc82f93def31681d802
royle <royle@tencent.com>
2021-07-05 16:52:11 +0800

    增加测试用例

---
+++ wxpic/choppy/resource/no_interest_conf.js
@@ -0,0 +1,13 @@
+try {
+  window.__PAGEJSCONF__ = window.__PAGEJSCONF__ || {}
+  window.__PAGEJSCONF__['no_interest_list'] = [{
+      label: '与我无关',
+      value: 1
+  },{
+      label: '重复收到多次',
+      value: 5
+  },{
+      label: '内容太差',
+      value: 4
+  }]
+} catch (e) {}
